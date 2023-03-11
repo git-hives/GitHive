ff70c9afeeeadf4b9f8be3d658504772ec1e7c8e
xiaohutu <shanxi.1991@gmail.com>
2023-03-11 11:19:14 +0800

    Initial Commit

---
+++ GitHive/GitHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHiveApp.swift
+//  GitHive
+//
+//  Created by 1 on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
